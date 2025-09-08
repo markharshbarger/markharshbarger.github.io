1d00b69fdf08f1a01da81bba381c7a79f78792dc
Wamski <waskyjo@gmail.com>
2025-09-07 21:45:16 -0400

    Updated CSS for mobile

---
+++ docs/mobileNav.js
@@ -0,0 +1,11 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamburger.classList.toggle('change');
+
+});
+
